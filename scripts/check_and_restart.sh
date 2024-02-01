900c0dae12784637ced7ed91cb5cdf37c0328a49
jsonjimenez <jeisonjimenes.1998@gmail.com>
2024-01-31 19:02:04 -0500

    update fix

---
+++ scripts/check_and_restart.sh
@@ -0,0 +1,8 @@
+# Verificar el estado del servicio
+if sudo systemctl is-active player.service | grep -q 'inactive'; then
+    # El servicio está pausado, iniciarlo
+    sudo systemctl start player.service
+else
+    # El servicio no está pausado
+    echo "El servicio player.service ya está en ejecución."
+fi
